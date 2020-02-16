7bff27e03a37c17367a6f3ed3053c4b9e1f9e90d
Unknown <adtsolanki22@gmail.com>
2020-02-16 17:34:33 +0530

    Added  class and style binding

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,15 @@
       <div class="row">
         <div class="column left">
             <img :title="image" v-bind:src="image" />
-            <!-- <img src="./images/img6.jpg"> -->
+            <!-- <img src="./images/img20.jpg"> -->
         </div>
         <div class="column right">
           <div class="product-info">
             <h1>{{ product }}</h1>
             <p v-if="inventory > 10">In Stock</p>
             <p v-else-if="inventory < 10 && inventory > 0 ">Almost Sold Out</p>
-            <p v-else>Out of Stock </p>
-            <p v-show="onSale">ON Sale</p>
+            <p v-else :class="[inventory == 0 ? textdecoration : '']">Out of Stock </p>
+            <p v-show="inventory > 0">ON Sale</p>
 
             <ul>
              <li :key="item" v-for="item in details">{{item}}</li>
@@ -25,8 +25,12 @@
 
               <span>Available Colors: </span>
               <br/>
-                <div style="display:block" v-for="variant in variants" :key="variant.variantId">
-                  <span @mouseover="updateProduct(variant.variantImage)">{{variant.variantColor}}</span>
+                <div 
+                  class="colorbox" 
+                  :style="{backgroundColor: variant.variantColor}" 
+                  v-for="variant in variants" 
+                  :key="variant.variantId"
+                  @mouseover="updateProduct(variant.variantImage)">
                 </div>
         
             <ul>
@@ -37,7 +41,7 @@
             </ul>
 
             <div>
-              <button v-on:click="addToCart">Add to cart</button>
+              <button :disabled="inventory <= 0" v-on:click="addToCart">Add to cart</button>
               <div class="cart">
                 Cart: {{cart}}
                 <button v-on:click="increaseProduct" style="pdding:5px;margin:5px">+</button>
@@ -65,7 +69,8 @@ export default {
       product: 'Dress',
       image: '/img/img1.bb990836.jpg',
       inventory: 0,
-      onSale: true,
+      //onSale: true,
+      textdecoration: 'line',
       details: ["80% cotton","20% polyester","gender-neutral"],
       variants: [
               {variantId:4,
@@ -75,6 +80,14 @@ export default {
               {variantId:6,
                 variantColor:"white",
                 variantImage:"/img/img6.f519008f.jpg"
+              },
+              {variantId:13,
+                variantColor:"blue",
+                variantImage:"/img/img13.bbd56ccc.jpg"
+              },
+              {variantId:20,
+                variantColor:"gray",
+                variantImage:"/img/img20.1d566e03.jpg"
               }
             ],
     sizes: ['xs','x','s','m','l','xl'],
