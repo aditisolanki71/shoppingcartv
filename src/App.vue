d882f28109aa9cc736fc83065fc3688fc83970f2
Unknown <adtsolanki22@gmail.com>
2020-02-16 16:02:49 +0530

    Added size,variants and details of product

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,25 @@
             <p v-else-if="inventory < 10 && inventory > 0 ">Almost Sold Out</p>
             <p v-else>Out of Stock </p>
             <p v-show="onSale">ON Sale</p>
+
+            <ul>
+             <li :key="item" v-for="item in details">{{item}}</li>
+            </ul>
+
+            <ul>
+              <span>Available Colors: </span>
+                <li v-for="variant in variants" :key="variant.variantId">
+                  {{variant.variantColor}}
+                </li>
+            </ul>
+
+            <ul>
+              <span>Available Sizes: </span>
+                <li style="display:inline;" v-for="size in sizes" :key="size">
+                  <span style="padding:5px">{{size}}</span>
+                </li>
+            </ul>
+
           </div>
         </div>
       </div>
@@ -39,7 +58,12 @@ export default {
       product: 'Dress',
       image: '/img/img1.bb990836.jpg',
       inventory: 0,
-      onSale: true
+      onSale: true,
+      details: ["80% cotton","20% polyester","gender-neutral"],
+      variants: [
+              {variantId:1,variantColor:"green"},
+              {variantId:2,variantColor:"red"}],
+    sizes: ['xs','x','s','m','l','xl']
     }
   }
 
